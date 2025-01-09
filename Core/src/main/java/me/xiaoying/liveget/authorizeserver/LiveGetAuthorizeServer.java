e65d4bd0f1091ef457cb47cc4759cafc90c58abf
hanhan2001 <764932129@qq.com>
2025-01-09 22:32:26 +0800

    feat: register FileConfig to read file which named "Configuration.yml"

--- Core/src/main/java/me/xiaoying/liveget/authorizeserver/LiveGetAuthorizeServer.java
+++ Core/src/main/java/me/xiaoying/liveget/authorizeserver/LiveGetAuthorizeServer.java
@@ -1,14 +1,29 @@
 package me.xiaoying.liveget.authorizeserver;
 
+import me.xiaoying.liveget.authorizeserver.file.FileConfig;
+import me.xiaoying.liveget.authorizeserver.file.FileManager;
+import me.xiaoying.liveget.authorizeserver.file.SimpleFileManager;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiveGetAuthorizeServer {
+    private static FileManager fileManager;
+
     public static void main(String[] args) {
+        // initialize
+        LACore.getLogger().info("Initialize...");
+        LiveGetAuthorizeServer.initialize();
+
         SpringApplication springApplication = new SpringApplication(LiveGetAuthorizeServer.class);
 
         springApplication.setBannerMode(Banner.Mode.OFF);
+    }
+
+    public static void initialize() {
+        // file
+        LiveGetAuthorizeServer.fileManager = new SimpleFileManager();
+        LiveGetAuthorizeServer.fileManager.register(new FileConfig());
     }
 }
---
+++ Core/src/main/java/me/xiaoying/liveget/authorizeserver/file/FileConfig.java
@@ -0,0 +1,78 @@
+package me.xiaoying.liveget.authorizeserver.file;
+
+public class FileConfig extends SFile {
+    public static String SERVER_HOST;
+    public static int SERVER_PORT;
+
+    public static String SETTING_JWT_KEY_ID,
+            SETTING_JWT_PUBLIC_KEY,
+            SETTING_JWT_PRIVATE_KEY;
+
+    public static int SETTING_CACHE_USER_TIME, SETTING_CACHE_TOKEN_TIME;
+    public static boolean SETTING_CACHE_USER_SAVE, SETTING_CACHE_TOKEN_SAVE;
+
+    public static String SETTING_PASSWORD_ENCRYPT, SETTING_PASSWORD_PASSWORD;
+
+    public static int SETTING_DATA_CACHE_TIME;
+
+    public static String SETTING_DATEFORMAT;
+
+    public static String SETTING_DATA_TYPE,
+            SETTING_DATA_MYSQL_HOSTNAME,
+            SETTING_DATA_MYSQL_DATABASE,
+            SETTING_DATA_MYSQL_USERNAME,
+            SETTING_DATA_MYSQL_PASSWORD;
+    public static int SETTING_DATA_MYSQL_PORT,
+            SETTING_DATA_MYSQL_RECONNECT_TIME,
+            SETTING_DATA_MYSQL_RECONNECT_DELAY;
+
+    public static String SETTING_DATA_SQLITE;
+
+    public FileConfig() {
+        super("Configuration.yml");
+    }
+
+    @Override
+    public void onLoad() {
+        FileConfig.SERVER_HOST = this.getConfiguration().getString("Server.Host");
+        FileConfig.SERVER_PORT = this.getConfiguration().getInt("Server.Port");
+
+        FileConfig.SETTING_JWT_KEY_ID = this.getConfiguration().getString("Setting.Jwt.KeyId");
+        FileConfig.SETTING_JWT_PUBLIC_KEY = this.getConfiguration().getString("Setting.Jwt.PublicKey");
+        FileConfig.SETTING_JWT_PRIVATE_KEY = this.getConfiguration().getString("Setting.Jwt.PrivateKey");
+
+        FileConfig.SETTING_CACHE_USER_TIME = this.getConfiguration().getInt("Setting.Cache.User.Time");
+        FileConfig.SETTING_CACHE_USER_SAVE = this.getConfiguration().getBoolean("Setting.Cache.User.Save");
+
+        FileConfig.SETTING_CACHE_TOKEN_TIME = this.getConfiguration().getInt("Setting.Cache.Token.Time");
+        FileConfig.SETTING_CACHE_TOKEN_SAVE = this.getConfiguration().getBoolean("Setting.Cache.Token.Save");
+
+        FileConfig.SETTING_PASSWORD_ENCRYPT = this.getConfiguration().getString("Setting.Password.Encrypt");
+        FileConfig.SETTING_PASSWORD_PASSWORD = this.getConfiguration().getString("Setting.Password.Password");
+
+        FileConfig.SETTING_DATA_CACHE_TIME = this.getConfiguration().getInt("Setting.DataCacheTime");
+
+        FileConfig.SETTING_DATEFORMAT = this.getConfiguration().getString("Setting.DateFormat");
+
+        FileConfig.SETTING_DATA_TYPE = this.getConfiguration().getString("Setting.Data.Type");
+        FileConfig.SETTING_DATA_MYSQL_HOSTNAME = this.getConfiguration().getString("Setting.Data.Mysql.Hostname");
+        FileConfig.SETTING_DATA_MYSQL_DATABASE = this.getConfiguration().getString("Setting.Data.Mysql.Database");
+        FileConfig.SETTING_DATA_MYSQL_USERNAME = this.getConfiguration().getString("Setting.Data.Mysql.Username");
+        FileConfig.SETTING_DATA_MYSQL_PASSWORD = this.getConfiguration().getString("Setting.Data.Mysql.Password");
+        FileConfig.SETTING_DATA_MYSQL_PORT = this.getConfiguration().getInt("Setting.Data.Mysql.Port");
+        FileConfig.SETTING_DATA_MYSQL_RECONNECT_TIME = this.getConfiguration().getInt("Setting.Data.Mysql.ReconnectTime");
+        FileConfig.SETTING_DATA_MYSQL_RECONNECT_DELAY = this.getConfiguration().getInt("Setting.Data.Mysql.ReconnectDelay");
+
+        FileConfig.SETTING_DATA_SQLITE = this.getConfiguration().getString("Setting.Data.Sqlite");
+    }
+
+    @Override
+    public void onDelete() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
