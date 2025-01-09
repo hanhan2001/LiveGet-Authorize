e1749ab2cf2551311e92101f9c2819d4aee33d16
hanhan2001 <764932129@qq.com>
2025-01-09 22:46:57 +0800

    feat: remove SqlFactory in LACore.java

--- API/src/main/java/me/xiaoying/liveget/authorizeserver/LACore.java
+++ API/src/main/java/me/xiaoying/liveget/authorizeserver/LACore.java
@@ -1,13 +1,10 @@
 package me.xiaoying.liveget.authorizeserver;
 
 import me.xiaoying.logger.Logger;
-import me.xiaoying.sql.SqlFactory;
 
 import java.io.File;
 
 public class LACore {
-    private static SqlFactory sqlFactory = null;
-
     private static final Logger logger = new Logger();
 
     public static File getDataFolder() {
@@ -16,16 +13,5 @@ public class LACore {
 
     public static Logger getLogger() {
         return LACore.logger;
-    }
-
-    public static void setSqlFactory(SqlFactory sqlFactory) {
-        if (LACore.sqlFactory != null)
-            return;
-
-        LACore.sqlFactory = sqlFactory;
-    }
-
-    public static SqlFactory gegSqlFactory() {
-        return LACore.sqlFactory;
     }
 }
