df7081308328c6a2c8d8e7e143ce535e02f32cf4
hanhan2001 <764932129@qq.com>
2025-01-09 22:47:32 +0800

    feat: make a new method of LiveGetAuthorizeServer#getSqlfactory

--- Core/src/main/java/me/xiaoying/liveget/authorizeserver/LiveGetAuthorizeServer.java
+++ Core/src/main/java/me/xiaoying/liveget/authorizeserver/LiveGetAuthorizeServer.java
@@ -3,9 +3,15 @@ package me.xiaoying.liveget.authorizeserver;
 import me.xiaoying.liveget.authorizeserver.file.FileConfig;
 import me.xiaoying.liveget.authorizeserver.file.FileManager;
 import me.xiaoying.liveget.authorizeserver.file.SimpleFileManager;
+import me.xiaoying.sql.MysqlFactory;
+import me.xiaoying.sql.SqlFactory;
+import me.xiaoying.sql.SqliteFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.util.Locale;
 
 @SpringBootApplication
 public class LiveGetAuthorizeServer {
@@ -27,8 +33,13 @@ public class LiveGetAuthorizeServer {
         // file
         LiveGetAuthorizeServer.fileManager = new SimpleFileManager();
         LiveGetAuthorizeServer.fileManager.register(new FileConfig());
+    }
 
-        // SqlFactory
-//        switch (FileConfig.)
+    public static SqlFactory getSqlFactory() {
+        return switch (FileConfig.SETTING_DATA_TYPE.toUpperCase(Locale.ENGLISH)) {
+            case "MYSQL" -> new MysqlFactory(FileConfig.SETTING_DATA_MYSQL_HOSTNAME, FileConfig.SETTING_DATA_MYSQL_PORT, FileConfig.SETTING_DATA_MYSQL_DATABASE, FileConfig.SETTING_DATA_MYSQL_USERNAME, FileConfig.SETTING_PASSWORD_PASSWORD);
+            case "SQLITE" -> new SqliteFactory(new File(FileConfig.SETTING_DATA_SQLITE));
+            default -> null;
+        };
     }
 }
