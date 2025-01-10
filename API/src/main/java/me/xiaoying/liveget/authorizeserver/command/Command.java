d95a9dfbc16a15586d825a7f04d84b3e0bf23e3b
hanhan2001 <764932129@qq.com>
2025-01-10 10:36:04 +0800

    feat: Command, make a new method of CommandManager#registerCommand and make notes

---
+++ API/src/main/java/me/xiaoying/liveget/authorizeserver/command/Command.java
@@ -0,0 +1,61 @@
+package me.xiaoying.liveget.authorizeserver.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Command {
+    private final String name;
+    private final String description;
+    private final String usage;
+    private final List<String> alias;
+
+    public Command(String name, String description, String usage) {
+        this.name = name;
+        this.description = description;
+        this.usage = usage;
+        this.alias = new ArrayList<>();
+    }
+
+    public Command(String name, String description, String usage, List<String> alias) {
+        this.name = name;
+        this.description = description;
+        this.usage = usage;
+        this.alias = alias;
+    }
+
+    /**
+     * Get command's name
+     *
+     * @return name
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Get command's description
+     *
+     * @return description
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Get command's usage
+     *
+     * @return usage
+     */
+    public String getUsage() {
+        return this.usage;
+    }
+
+    /**
+     * Get command's alias
+     *
+     * @return alias
+     */
+    public List<String> getAlias() {
+        return this.alias;
+    }
+}
--- API/src/main/java/me/xiaoying/liveget/authorizeserver/command/CommandManager.java
+++ API/src/main/java/me/xiaoying/liveget/authorizeserver/command/CommandManager.java
@@ -1,4 +1,19 @@
 package me.xiaoying.liveget.authorizeserver.command;
 
 public interface CommandManager {
+    /**
+     * Register command
+     *
+     * @param fallbackPrefix command's prefix
+     * @param command Command
+     */
+    void registerCommand(String fallbackPrefix, Command command);
+
+    /**
+     * Perform command
+     *
+     * @param command string
+     * @return Determine command work
+     */
+    boolean dispatch(String command);
 }
