ad49d0d22b5a5f5610fa041a38d1a87e164d6571
hanhan2001 <764932129@qq.com>
2025-01-13 19:20:10 +0800

    feat: add notes for ScheduledManager

--- API/src/main/java/me/xiaoying/liveget/authorizeserver/scheduler/ScheduledManager.java
+++ API/src/main/java/me/xiaoying/liveget/authorizeserver/scheduler/ScheduledManager.java
@@ -1,5 +1,10 @@
 package me.xiaoying.liveget.authorizeserver.scheduler;
 
 public interface ScheduledManager {
+    /**
+     * Stop task
+     *
+     * @param task Task's id
+     */
     void cancelTask(int task);
 }
