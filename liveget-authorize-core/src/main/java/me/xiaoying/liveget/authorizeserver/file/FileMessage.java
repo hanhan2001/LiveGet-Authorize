ab448426aa23ccb5b1a4159ae04d3e977d4c0014
hanhan2001 <764932129@qq.com>
2025-03-07 14:52:56 +0800

    feat: add option of "UserInfo" and "UserCreateSuccess"

--- liveget-authorize-core/src/main/java/me/xiaoying/liveget/authorizeserver/file/FileMessage.java
+++ liveget-authorize-core/src/main/java/me/xiaoying/liveget/authorizeserver/file/FileMessage.java
@@ -4,10 +4,13 @@ package me.xiaoying.liveget.authorizeserver.file;
  * Message.yml
  */
 public class FileMessage extends SFile {
+    public static String USER_INFO;
+
     public static String USER_CREATE_FREQUENTLY,
             USER_CREATE_EXISTS_EMAIL,
             USER_CREATE_EXISTS_PHONE,
-            USER_CREATE_WRONG_PASSWORD;
+            USER_CREATE_WRONG_PASSWORD,
+            USER_CREATE_CREATE_SUCCESS;
 
     public static String PARAMETER_MISSING_PARAMETER;
 
@@ -17,10 +20,13 @@ public class FileMessage extends SFile {
 
     @Override
     public void onLoad() {
+        FileMessage.USER_INFO = this.getConfiguration().getString("User.Info");
+
         FileMessage.USER_CREATE_FREQUENTLY = this.getConfiguration().getString("User.Create.Frequently");
         FileMessage.USER_CREATE_EXISTS_EMAIL = this.getConfiguration().getString("User.Create.ExistsEmail");
         FileMessage.USER_CREATE_EXISTS_PHONE = this.getConfiguration().getString("User.Create.ExistsPhone");
         FileMessage.USER_CREATE_WRONG_PASSWORD = this.getConfiguration().getString("User.Create.WrongPassword");
+        FileMessage.USER_CREATE_CREATE_SUCCESS = this.getConfiguration().getString("User.Create.CreateSuccess");
 
         FileMessage.PARAMETER_MISSING_PARAMETER = this.getConfiguration().getString("Parameter.MissingParameter");
     }
